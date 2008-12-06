0280f1ad66a82ba1f7771cacc76dc35d56ca6cfb
jeffrey.heer <jeffrey.heer@35a52bc0-c39b-11dd-bf30-1748d1f6ef2f>
2008-12-06 14:28:49 +0000

    Added DataSet inheritance

--- trunk/obvious/src/obvious/Graph.java
+++ trunk/obvious/src/obvious/Graph.java
@@ -22,7 +22,7 @@ import java.util.Collection;
 //(2) Node and edge types?
 //(3) Explicit Table-related methods?
 
-public interface Graph<V, E, T>
+public interface Graph<V, E, T> implements DataSet
 {
     Collection<E> getEdges();
     Collection<V> getNodes();
