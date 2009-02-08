5688b38c5fa2edeaa9858cbecf269e1811c47a3a
mike.smoot <mike.smoot@35a52bc0-c39b-11dd-bf30-1748d1f6ef2f>
2009-02-08 01:15:51 +0000

    Added an initial implementation of Graph using Cytoscape's CyNetwork and added a basic unit testing framework that can be shared by different implementations of Graph.

---
+++ obvious-abstract-test/src/main/java/obvious/test/AbstractGraphTest.java
@@ -0,0 +1,28 @@
+
+package obvious.test;
+
+import obvious.Graph;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public abstract class AbstractGraphTest {
+
+	protected Graph<String,String> graph;
+
+	@Test
+	public void testAddNode() {
+		assertTrue( graph.addNode("homer") );
+		assertTrue( graph.addNode("marge") );
+	}
+
+	@Test
+	public void testAddEdge() {
+		graph.addNode("a");
+		graph.addNode("b");
+		graph.addNode("c");
+		assertTrue( graph.addEdge("ab-dir","a","b",Graph.EdgeType.DIRECTED) );
+		assertTrue( graph.addEdge("bc-undir","b","c",Graph.EdgeType.UNDIRECTED) );
+	}
+}
+
