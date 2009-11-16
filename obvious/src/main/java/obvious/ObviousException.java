12029ee3bdffca7a3f464d8ed0382c72888b72fa
jean-daniel.fekete@laposte.net <jean-daniel.fekete@laposte.net@35a52bc0-c39b-11dd-bf30-1748d1f6ef2f>
2009-11-16 16:17:57 +0000

    Define exception for obvious

---
+++ obvious/src/main/java/obvious/ObviousException.java
@@ -0,0 +1,44 @@
+
+package obvious;
+
+/**
+ * Class ObviousException
+ * 
+ * @author Jean-Daniel Fekete
+ * @version $Revision$
+ */
+public class ObviousException extends Exception {
+
+    /**
+     * 
+     */
+    public ObviousException() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param arg0
+     */
+    public ObviousException(String arg0) {
+        super(arg0);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param arg0
+     */
+    public ObviousException(Throwable arg0) {
+        super(arg0);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param arg0
+     * @param arg1
+     */
+    public ObviousException(String arg0, Throwable arg1) {
+        super(arg0, arg1);
+        // TODO Auto-generated constructor stub
+    }
+
+}
