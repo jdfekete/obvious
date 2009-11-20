ad2d08437749086b50d5efe6d5214b6ce04bd01b
pl <pl@35a52bc0-c39b-11dd-bf30-1748d1f6ef2f>
2009-11-20 13:25:35 +0000

    Add unit test for Table Class (with its Data class) Add unit test for Schema Class

---
+++ obvious/src/test/java/test/obvious/data/SchemaTest.java
@@ -0,0 +1,144 @@
+/*
+* Copyright (c) 2009, INRIA
+* All rights reserved.
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of INRIA nor the names of its contributors may
+*       be used to endorse or promote products derived from this software 
+*       without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package test.obvious.data;
+
+import obvious.data.Schema;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Test class for Schema Interface.
+ * @author Pierre-Luc Hemery
+ *
+ */
+public class SchemaTest {
+
+  /**
+   * Schema instance for tests.
+   */
+  private Schema schema;
+
+  /**
+   * @see junit.framework.TestCase#setUp()
+   */
+  @Before
+  public void setUp() {
+      schema.addColumn("col1", String.class, "");
+      schema.addColumn("col2", Integer.class, 0);
+  }
+
+  /**
+   * @see junit.framework.TestCase#tearDown()
+   */
+  @After
+  public void tearDown() {
+    schema = null;
+  }
+
+  /**
+   * Test method for obvious.data.Schema.getColumnCount() method.
+   */
+  @Test
+  public void testGetColumnCount() {
+    assertEquals(2, schema.getColumnCount());
+  }
+
+  /**
+   * Test method for obvious.data.Schema.getColumnType(int col) method.
+   */
+  @Test
+  public void testGetColumnTypeByIndex() {
+    assertEquals(String.class, schema.getColumnType(0));
+    assertEquals(Integer.class, schema.getColumnType(1));
+  }
+
+  /**
+   * Test method for obvious.data.Schema.getColumnDefault(string field) method.
+   */
+  @Test
+  public void testGetColumnTypeByField() {
+    assertEquals(String.class, schema.getColumnType("col1"));
+    assertEquals(Integer.class, schema.getColumnType("col2"));
+  }
+
+  /**
+   * Test method for obvious.data.Schema.getColumnDefault(int col) method.
+   */
+  @Test
+  public void testGetColumnDefault() {
+    assertEquals("", schema.getColumnDefault(0));
+    assertEquals(0, schema.getColumnDefault(1));
+  }
+
+  /**
+   * Test method for obvious.data.Schema.getColumnName(int col) method.
+   */
+  @Test
+  public void testGetColumnName() {
+    assertEquals("col1", schema.getColumnName(0));
+    assertEquals("col2", schema.getColumnName(1));
+  }
+
+  /**
+   * Test method for obvious.data.Schema.getColumnIndex(String field) method.
+   */
+  @Test
+  public void testGetColumnIndex() {
+    assertEquals(0, schema.getColumnIndex("col1"));
+    assertEquals(1, schema.getColumnIndex("col2"));
+  }
+
+  /**
+   * Test method for obvious.data.Schema.hasColumn(string field) method.
+   */
+  public void testHasColumn() {
+    assertTrue(schema.hasColumn("col1"));
+    assertTrue(schema.hasColumn("col2"));
+    assertFalse(schema.hasColumn("foooo"));
+  }
+
+  /**
+   * Test method for obvious.data.Schema.removeColumn(int col) method.
+   */
+  @Test
+  public void testRemoveColumnByIndex() {
+    assertTrue(schema.removeColumn(0));
+    assertTrue(schema.removeColumn(1));
+    assertFalse(schema.removeColumn(3));
+  }
+
+  /**
+   * Test method for obvious.data.Schema.removeColumn(String field) method.
+   */
+  public void testRemoveColumnByField() {
+    assertTrue(schema.removeColumn("col1"));
+    assertTrue(schema.removeColumn("col2"));
+    assertFalse(schema.removeColumn("foo"));
+  }
+}
---
+++ obvious/src/test/java/test/obvious/data/TableTest.java
@@ -0,0 +1,184 @@
+/*
+* Copyright (c) 2009, INRIA
+* All rights reserved.
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of INRIA nor the names of its contributors may
+*       be used to endorse or promote products derived from this software
+*       without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package test.obvious.data;
+
+import obvious.ObviousException;
+import obvious.data.Table;
+import obvious.data.DataFactory;
+import obvious.impl.SchemaImpl;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Class TableTest.
+ *
+ * @author Pierre-Luc Hemery
+ * @version $Revision$
+ */
+
+public class TableTest implements TableTestData {
+
+  private Table table;
+
+  /**
+  * @see junit.framework.TestCase#setUp()
+  * @throws ObviousException
+  */
+  @Before
+  public void setUp() throws ObviousException {
+    SchemaImpl  schema = new SchemaImpl();
+    for (int i = 0; i < NUMCOL; i++) {
+      schema.addColumn(HEADERS[i], TYPES[i], DEFAULTS[i]);
+    }
+    DataFactory factory = DataFactory.getInstance();
+    Table table = factory.createTable("table", schema);
+    table.addRow();
+    table.addRow();
+    table.addRow();
+    table.addRow();
+    for (int i = 0; i < NUMROW; i++) {
+      table.set(i, 0, COLUMN1[i]);
+      table.set(i, 1, COLUMN2[i]);
+      table.set(i, 2, COLUMN3[i]);
+    }
+  }
+
+  /**
+  * @see junit.framework.TestCase#tearDown()
+  */
+  @After
+  public void tearDown() {
+    table = null;
+  }
+
+// Read-only test
+
+  /**
+   * Test method for obvious.data.Table.getRowCount() method.
+   */
+  @Test
+  public void testGetRowCount() {
+    assertEquals(NUMROW, table.getRowCount());
+  }
+
+  /**
+  * Test method for obvious.data.Table.isValidRow() method.
+  */
+  @Test
+  public void testIsValidRow() {
+    assertTrue(table.isValidRow(0));
+    assertTrue(table.isValidRow(2));
+    assertFalse(table.isValidRow(555));
+  }
+
+  /**
+  * Test method for obvious.data.Table.isValueValid() method.
+  */
+  @Test
+  public void isValueValid() {
+    assertTrue(table.isValueValid(0, 0));
+    assertFalse(table.isValueValid(4, 4));
+  }
+
+  /**
+  * Test method for obvious.data.Table.getValue() method.
+  */
+  @Test
+  public void testGetValue() {
+    assertEquals("Hello", table.getValue(1, 0));
+    assertEquals("Hello", table.getValue(1, "string"));
+    assertEquals(1, table.getValue(0, 1));
+    assertEquals(1, table.getValue(0, "integer"));
+    assertEquals(true, table.getValue(3, 2));
+    assertEquals(true, table.getValue(3, "boolean"));
+  }
+
+// Read-write test
+
+  /**
+  * Test method for obvious.data.Table.addRow() method.
+  */
+  @Test
+  public void testAddRow() {
+    table.addRow();
+    assertEquals(NUMROW + 1, table.getRowCount());
+  }
+
+  /**
+  * Test method for obvious.data.Table.removeAllRows() method.
+  */
+  @Test
+  public void removeAllRows() {
+    table.removeAllRows();
+    assertEquals(0, table.getRowCount());
+  }
+
+  /**
+  * Test method for obvious.data.Table.set() method.
+  */
+  @Test
+  public void testSet() {
+    table.set(0, 0, "hey");
+    assertEquals("hey", table.getValue(0, 0));
+  }
+
+  /**
+   * Test method for obvious.data.Table.canRemoveRow()
+   * At the moment, not enough information about the method to write unit test.
+   */
+  @Test
+  public void testCanRemoveRow() {
+  }
+
+  /**
+   * Test method for obvious.data.Table.canRemoveRow()
+   * At the moment, not enough information about the method to write unit test.
+   */
+  @Test
+  public void testCanAddRow() {
+  }
+
+  /**
+  * Test method for testing isolation i.e.
+  * obvious.data.Table.beginEdit(),isEditing() and endEdit()
+  */
+  @Test
+  public void testIsolation() {
+    for (int i = 0; i < NUMROW; i++) {
+      assertFalse(table.isEditing(i));
+    }
+    try {
+      table.beginEdit(0);
+      assertTrue(table.isEditing(0));
+    } finally {
+      table.endEdit(0);
+    }
+  }
+
+}
