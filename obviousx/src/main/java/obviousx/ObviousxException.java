5fd35798d76aa917c9ade6f81489e4a43f19c299
pl <pl@35a52bc0-c39b-11dd-bf30-1748d1f6ef2f>
2009-11-26 08:55:25 +0000

    Creating basic package hierarchy for Obviousx project. Import checkstyle rules.

---
+++ obviousx/src/main/java/obviousx/ObviousxException.java
@@ -0,0 +1,74 @@
+/*
+* Copyright (c) 2009, INRIA
+* All rights reserved.
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of INRIA nor the names of its contributors may
+*       be used to endorse or promote products derived from this software
+*       without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package obviousx;
+
+/**
+ *
+ * @author Pierre-Luc Hémery
+ *
+ */
+public class ObviousxException extends Exception {
+
+  /**
+   * Serial ID.
+   */
+  private static final long serialVersionUID = -7797405537771921590L;
+
+  /**
+  *
+  */
+ public ObviousxException() {
+     // TODO Auto-generated constructor stub
+ }
+
+ /**
+  * @param arg0 message
+  */
+ public ObviousxException(String arg0) {
+     super(arg0);
+     // TODO Auto-generated constructor stub
+ }
+
+ /**
+  * @param arg0 Throwable
+  */
+ public ObviousxException(Throwable arg0) {
+     super(arg0);
+     // TODO Auto-generated constructor stub
+ }
+
+ /**
+  * @param arg0 message
+  * @param arg1 Throwable
+  */
+ public ObviousxException(String arg0, Throwable arg1) {
+     super(arg0, arg1);
+     // TODO Auto-generated constructor stub
+ }
+
+}
