a3a516ab4e544cd640131400342fe6530e25f3ff
pl <pl@35a52bc0-c39b-11dd-bf30-1748d1f6ef2f>
2010-01-15 10:15:03 +0000

    [obvious]Rewriting of NetworkTest class. [obvious]Rewriting of TreeTest class.

--- obvious/src/test/java/test/obvious/data/NetworkTest.java
+++ obvious/src/test/java/test/obvious/data/NetworkTest.java
@@ -34,7 +34,14 @@ import obvious.ObviousException;
 import obvious.data.Edge;
 import obvious.data.Network;
 import obvious.data.Node;
+import obvious.data.Schema;
+import obvious.data.Table;
+import obvious.data.Tree;
 import obvious.data.Graph.EdgeType;
+import obvious.impl.EdgeImpl;
+import obvious.impl.NodeImpl;
+import obvious.impl.SchemaImpl;
+import obvious.impl.TableImpl;
 
 
 import org.junit.Before;
@@ -57,9 +64,24 @@ public abstract class NetworkTest {
   private Network network;
 
   /**
-   * Number of nodes of the network test instance.
+   * Number of nodes of the test network.
    */
-  private static final int NUMNODE = 2;
+  private static final int NODENUMBER = 7;
+
+  /**
+   * Number of edges of the test network.
+   */
+  private static final int EDGENUMBER = 6;
+
+  /**
+   * Node table used to build the example tree.
+   */
+  private Table nodeTable;
+
+  /**
+   * Edge table uses to build the example tree.
+   */
+  private Table edgeTable;
 
   /**
    * Gets the network test instance.
@@ -75,132 +97,34 @@ public abstract class NetworkTest {
    */
   @Before
   public void setUp() throws ObviousException {
-    this.network = this.newInstance(NUMNODE);
-  }
-
-  /**
-   * Creates a suitable instance of network.
-   * This Network should at least have one edge and two nodes.
-   * @param numNode number of nodes of the graph.
-   * @return suitable network implementation instance
-   * @throws ObviousException if Network instantiation fails
-   */
-  public abstract Network newInstance(int numNode) throws ObviousException;
-
-  /**
-   * Creates a new instance of Node.
-   * This method should be used to build Node instances that match your
-   * Obvious implementation when you need to introduce node in your test.
-   * It must generate a node that is not already in the network.
-   * @return an Obvious Node
-   */
-  public abstract Node newNode();
-
-  /**
-   * Creates a new instance of Edge.
-   * This method should be used to build Node instances that match your
-   * Obvious implementation when you need to introduce edge in your test.
-   * It must generate an edge that is not already in the network.
-   * @return an Obvious Edge
-   */
-  public abstract Edge newEdge();
-
-  /**
-   * Test method for obvious.data.Network.addNode(Node) method.
-   */
-  @Test
-  public void testAddNode() {
-    Node addableNode = this.newNode();
-    assertTrue(this.network.addNode(addableNode));
-  }
-
-  /**
-   * Test method for obvious.data.Network.removeNode(Node) method.
-   */
-  @Test
-  public void testRemoveNode() {
-    Node externalNode = this.newNode();
-    Node internalNode = this.network.getNodes().iterator().next();
-    assertFalse(this.network.removeNode(externalNode));
-    assertTrue(this.network.removeNode(internalNode));
+    Schema nodeSchema = new SchemaImpl();
+    nodeSchema.addColumn("nodeName", String.class, "node_default");
+    Schema edgeSchema = new SchemaImpl();
+    edgeSchema.addColumn("edgeName", String.class, "edge_default");
+    nodeTable = new TableImpl(nodeSchema);
+    edgeTable = new TableImpl(edgeSchema);
+    this.network = this.newInstance(nodeSchema, edgeSchema);
+    for (int i = 0; i < NODENUMBER; i++) {
+      nodeTable.addRow();
+      nodeTable.set(i, 0, "node_" + String.valueOf(i));
+      this.network.addNode(new NodeImpl(nodeTable, i));
     }
-
-  /**
-   * Test method for obvious.data.Network.addEdge(Edge,Node,Node) method.
-   */
-  @Test
-  public void testAddEge() {
-    Edge addableEdge = this.newEdge();
-    Node[] node = new Node[2];
-    Iterator<Node> it = this.network.getNodes().iterator();
-    int count = 0;
-    while (it.hasNext()) {
-      if (count < 2) {
-        node[count] = it.next();
-      }
-      count++;
+    for (int i = 0; i < EDGENUMBER; i++) {
+      edgeTable.addRow();
+      edgeTable.set(i, "edgeName", "edge_" + String.valueOf(i));
     }
-    int numEdge = this.network.getEdges().size();
-    assertTrue(this.network.addEdge(addableEdge, node[0], node[1],
-        EdgeType.UNDIRECTED));
-    assertEquals(numEdge + 1, this.network.getEdges().size());
-  }
-
-  /**
-   * Test method for obvious.data.Network.removeEdge(Edge) method.
-   */
-  @Test
-  public void testRemoveEdge() {
-    Edge externalEdge = this.newEdge();
-    Edge internalEdge = this.network.getEdges().iterator().next();
-    assertFalse(this.network.removeEdge(externalEdge));
-    assertTrue(this.network.removeEdge(internalEdge));
-  }
-
-  /**
-   * Test method for obvious.data.Network.getConnectingEdge(Node, Node).
-   */
-  @Test
-  public void testGetConnectingEdge() {
-    Node node1 = this.newNode();
-    this.network.addNode(node1);
-    Node node2 = this.newNode();
-    this.network.addNode(node2);
-    Edge edge = this.newEdge();
-    this.network.addEdge(edge, node1, node2, EdgeType.DIRECTED);
-    assertEquals(edge, this.network.getConnectingEdge(node1, node2));
-  }
-
-  /**
-   * Test method for obvious.data.Network.getConnectingEdges(Node, Node).
-   */
-  @Test
-  public void testGetConnectingEdges() {
-    Node node1 = this.newNode();
-    this.network.addNode(node1);
-    Node node2 = this.newNode();
-    this.network.addNode(node2);
-    Edge edge1 = this.newEdge();
-    this.network.addEdge(edge1, node1, node2, EdgeType.DIRECTED);
-    Edge edge2 = this.newEdge();
-    this.network.addEdge(edge2, node1, node2, EdgeType.DIRECTED);
-    Collection<Edge> conEdge = this.network.getConnectingEdges(node1, node2);
-    assertEquals(2, conEdge.size());
-  }
-
-  /**
-   * Test method for obvious.data.Network.getOpposite(Node, Edge) method.
-   */
-  @Test
-  public void testGetOpposite() {
-    Node node1 = this.newNode();
-    this.network.addNode(node1);
-    Node node2 = this.newNode();
-    this.network.addNode(node2);
-    Edge edge = this.newEdge();
-    this.network.addEdge(edge, node1, node2, EdgeType.DIRECTED);
-    assertEquals(node1, this.network.getOpposite(node2, edge));
-    assertEquals(node2, this.network.getOpposite(node1, edge));
+    network.addEdge(new EdgeImpl(edgeTable, 0), new NodeImpl(nodeTable, 0),
+        new NodeImpl(nodeTable, 1), EdgeType.UNDIRECTED);
+    network.addEdge(new EdgeImpl(edgeTable, 1), new NodeImpl(nodeTable, 0),
+        new NodeImpl(nodeTable, 2), EdgeType.UNDIRECTED);
+    network.addEdge(new EdgeImpl(edgeTable, 2), new NodeImpl(nodeTable, 1),
+        new NodeImpl(nodeTable, 3), EdgeType.UNDIRECTED);
+    network.addEdge(new EdgeImpl(edgeTable,3), new NodeImpl(nodeTable, 1),
+        new NodeImpl(nodeTable, 4), EdgeType.UNDIRECTED);
+    network.addEdge(new EdgeImpl(edgeTable,4), new NodeImpl(nodeTable, 3),
+        new NodeImpl(nodeTable, 4), EdgeType.UNDIRECTED);
+    boolean net = network.addEdge(new EdgeImpl(edgeTable,5), new NodeImpl(nodeTable, 4),
+        new NodeImpl(nodeTable, 5), EdgeType.UNDIRECTED);
   }
 
   /**
@@ -209,6 +133,258 @@ public abstract class NetworkTest {
   @After
   public void tearDown() {
     this.network = null;
+    this.edgeTable = null;
+    this.nodeTable = null;
+  }
+
+  /**
+   * Creates a suitable instance of network.
+   * @param nSchema schema for the nodes the network
+   * @param eSchema schema for the edges of the network
+   * @return suitable network implementation instance
+   */
+  public abstract Network newInstance(Schema nSchema, Schema eSchema);
+
+  /**
+   * Test method for obvious.data.Network.getNodes() method.
+   */
+  @Test
+  public void testGetEdges() {
+    assertEquals(EDGENUMBER, this.network.getEdges().size());
+    int count = 0;
+    for (Edge edge : this.network.getEdges()) {
+      assertEquals("edge_" + count, edge.getString("edgeName"));
+      count++;
+    }
+  }
+
+  /**
+   * Test method for obvious.data.Network.getNodes() method.
+   */
+  @Test
+  public void testGetNodes() {
+    assertEquals(NODENUMBER, this.network.getNodes().size());
+    int count = 0;
+    for (Node node : this.network.getNodes()) {
+      assertEquals("node_" + count, node.getString("nodeName"));
+      count++;
+    }
+  }
+
+  /**
+   * Test method for obvious.data.Network.addNode(Node) method.
+   */
+  @Test
+  public void testAddNode() {
+    nodeTable.addRow();
+    nodeTable.set(nodeTable.getRowCount() - 1, "nodeName", "node_"
+        + NODENUMBER);
+    assertTrue(this.network.addNode(new NodeImpl(nodeTable, NODENUMBER)));
+    assertEquals(NODENUMBER + 1, this.network.getNodes().size());
+  }
+
+  /**
+   * Test method for obvious.data.Network.removeNode(Node) method.
+   */
+  @Test
+  public void testRemoveNode() {
+    nodeTable.addRow();
+    nodeTable.set(nodeTable.getRowCount() - 1, "nodeName", "node_"
+        + NODENUMBER);
+    // node not in the graph
+    assertFalse(network.removeNode(new NodeImpl(nodeTable, NODENUMBER)));
+    // node in the graph
+    assertTrue(network.removeNode(new NodeImpl(nodeTable, NODENUMBER - 1)));
+    assertEquals(NODENUMBER - 1, this.network.getNodes().size());
+  }
+
+  /**
+   * Test method for obvious.data.Network.addEdge(Edge,Node,Node) method.
+   */
+  @Test
+  public void testAddEge() {
+    edgeTable.addRow();
+    edgeTable.set(edgeTable.getRowCount() - 1, "edgeName", "edge_"
+        + EDGENUMBER);
+    assertTrue(network.addEdge(new EdgeImpl(edgeTable, EDGENUMBER),
+        new NodeImpl(nodeTable, 0), new NodeImpl(nodeTable, NODENUMBER - 1),
+        EdgeType.UNDIRECTED));
+    assertEquals(EDGENUMBER + 1, this.network.getEdges().size());
+  }
+
+  /**
+   * Test method for obvious.data.Network.removeEdge(Edge) method.
+   */
+  @Test
+  public void testRemoveEdge() {
+    edgeTable.addRow();
+    edgeTable.set(edgeTable.getRowCount() - 1, "edgeName", "edge_"
+        + EDGENUMBER);
+    // Edge not in the graph
+    assertFalse(network.removeEdge(new EdgeImpl(edgeTable, EDGENUMBER)));
+    // Edge in the graph
+    assertTrue(network.removeEdge(new EdgeImpl(edgeTable, EDGENUMBER - 1)));
+  }
+
+  /**
+   * Test method for obvious.data.Network.getConnectingEdge(Node, Node).
+   */
+  @Test
+  public void testGetConnectingEdge() {
+    // this method returns the firts connecting edge between two nodes.
+    // In this test, couple of nodes only have one edge between them. So,
+    // we can predict, the edge returned by the method.
+    assertEquals("edge_0", network.getConnectingEdge(new NodeImpl(nodeTable, 0),
+        new NodeImpl(nodeTable, 1)).get("edgeName"));
+    assertEquals("edge_1", network.getConnectingEdge(new NodeImpl(nodeTable, 0),
+        new NodeImpl(nodeTable, 2)).get("edgeName"));
+    assertEquals("edge_2", network.getConnectingEdge(new NodeImpl(nodeTable, 1),
+        new NodeImpl(nodeTable, 3)).get("edgeName"));
+    assertEquals("edge_3", network.getConnectingEdge(new NodeImpl(nodeTable, 1),
+        new NodeImpl(nodeTable, 4)).get("edgeName"));
+    assertEquals("edge_4", network.getConnectingEdge(new NodeImpl(nodeTable, 3),
+        new NodeImpl(nodeTable, 4)).get("edgeName"));
+    assertEquals("edge_5", network.getConnectingEdge(new NodeImpl(nodeTable, 4),
+        new NodeImpl(nodeTable, 5)).get("edgeName"));
+
+  }
+
+  /**
+   * Test method for obvious.data.Network.getConnectingEdges(Node, Node).
+   */
+  @Test
+  public void testGetConnectingEdges() {
+    // this method is useful for multigraphs. By default, our graph is not a
+    // multigraph. So, first, this test add an edge to an existing linked nodes
+    // couple to be able to test this method.
+    edgeTable.addRow();
+    edgeTable.set(edgeTable.getRowCount() - 1, "edgeName", "edge_"
+        + EDGENUMBER);
+    network.addEdge(new EdgeImpl(edgeTable, EDGENUMBER),
+        new NodeImpl(nodeTable, 1), new NodeImpl(nodeTable, 3),
+        EdgeType.UNDIRECTED);
+    // test begins here
+    assertEquals(0, network.getConnectingEdges(new NodeImpl(nodeTable, 0),
+        new NodeImpl(nodeTable, NODENUMBER - 1)).size());
+    assertEquals(1, network.getConnectingEdges(new NodeImpl(nodeTable, 0),
+        new NodeImpl(nodeTable, 1)).size());
+    assertEquals(1, network.getConnectingEdges(new NodeImpl(nodeTable, 1),
+        new NodeImpl(nodeTable, 0)).size());
+    assertEquals(2, network.getConnectingEdges(new NodeImpl(nodeTable, 1),
+        new NodeImpl(nodeTable, 3)).size());
+    Collection<Edge> conEdge = network.getConnectingEdges(new
+        NodeImpl(nodeTable, 1), new NodeImpl(nodeTable, 3));
+    for (Edge edge : conEdge) {
+      assertTrue(edge.get("edgeName").equals("edge_2") || edge.get(
+          "edgeName").equals("edge_" + EDGENUMBER));
+    }
+  }
+
+  /**
+   * Test method for obvious.data.Network.getOpposite(Node, Edge) method.
+   */
+  @Test
+  public void testGetOpposite() {
+    assertEquals("node_1", network.getOpposite(new NodeImpl(nodeTable, 0),
+        new EdgeImpl(edgeTable, 0)).get("nodeName"));
+    assertEquals("node_0", network.getOpposite(new NodeImpl(nodeTable, 1),
+        new EdgeImpl(edgeTable, 0)).get("nodeName"));
+  }
+
+  /**
+   * Test method for obvious.data.Network.getNeighbors(Node) method.
+   */
+  @Test
+  public void testGetNeighbors() {
+    assertEquals(2, network.getNeighbors(new NodeImpl(nodeTable, 0)).size());
+    assertEquals(3, network.getNeighbors(new NodeImpl(nodeTable, 1)).size());
+    assertEquals(1, network.getNeighbors(new NodeImpl(nodeTable, 2)).size());
+    assertEquals(3, network.getNeighbors(new NodeImpl(nodeTable, 4)).size());
+    Collection<Node> neighbr = network.getNeighbors(new NodeImpl(nodeTable, 0));
+    for (Node node : neighbr) {
+      String nodeName = node.getString("nodeName");
+      assertTrue(nodeName.equals("node_1") || nodeName.equals("node_2"));
+    }
+  }
+
+  /**
+   * Test method for obvious.data.Network.getIncidentEdges(Node) method.
+   */
+  @Test
+  public void testGetIncidentEdges() {
+    assertEquals(2, network.getIncidentEdges(new NodeImpl(
+        nodeTable, 0)).size());
+    assertEquals(3, network.getIncidentEdges(new NodeImpl(
+        nodeTable, 1)).size());
+    assertEquals(1, network.getIncidentEdges(new NodeImpl(
+        nodeTable, 2)).size());
+    assertEquals(3, network.getIncidentEdges(new NodeImpl(
+        nodeTable, 4)).size());
+    Collection<Edge> incident = network.getIncidentEdges(
+        new NodeImpl(nodeTable, 0));
+    for (Edge edge : incident) {
+      String edgeName = edge.getString("edgeName");
+      assertTrue(edgeName.equals("edge_0") || edgeName.equals("edge_1"));
+    }
+  }
+
+  /**
+   * Test method for obvious.data.Network.getIncidentNodes(Edge) method.
+   */
+  @Test
+  public void testGetIncidentNodes() {
+    assertEquals(2, network.getIncidentNodes(new EdgeImpl(
+        edgeTable, 0)).size());
+    assertEquals(2, network.getIncidentNodes(new EdgeImpl(
+        edgeTable, 1)).size());
+    Collection<Node> incident = network.getIncidentNodes(
+        new EdgeImpl(edgeTable, 0));
+    for (Node node : incident) {
+      String nodeName = node.getString("nodeName");
+      assertTrue(nodeName.equals("node_0") || nodeName.equals("node_1"));
+    }
+  }
+
+  /**
+   * Test method for obvious.data.Network.getSource(Edge) method.
+   */
+  @Test
+  public void testGetSource() {
+  }
+
+  /**
+   * Test method for obvious.data.Network.getTarget(Edge) method.
+   */
+  @Test
+  public void testGetTarget() {
+  }
+
+  /**
+   * Test method for obvious.data.Network.getPredecessors(Node) method.
+   */
+  @Test
+  public void testGetPredecessors() {
+  }
+
+  /**
+   * Test method for obvious.data.Network.getSuccessors(Node) method.
+   */
+  @Test
+  public void testGetSuccessors() {
+  }
+
+  /**
+   * Test method for obvious.data.Network.getInEdges(Node) method.
+   */
+  @Test
+  public void testGetInEdges() {
+  }
+
+  /**
+   * Test method for obvious.data.Network.getOutEdges(Node) method.
+   */
+  @Test
+  public void testGetOutEdges() {
   }
 
 }
--- obvious/src/test/java/test/obvious/data/TreeTest.java
+++ obvious/src/test/java/test/obvious/data/TreeTest.java
@@ -27,8 +27,19 @@
 
 package test.obvious.data;
 
-import obvious.data.Graph;
+import java.util.Collection;
+import java.util.Iterator;
+
+import obvious.data.Edge;
+import obvious.data.Node;
+import obvious.data.Schema;
+import obvious.data.Table;
 import obvious.data.Tree;
+import obvious.data.Graph.EdgeType;
+import obvious.impl.EdgeImpl;
+import obvious.impl.NodeImpl;
+import obvious.impl.SchemaImpl;
+import obvious.impl.TableImpl;
 
 import org.junit.Test;
 import org.junit.Before;
@@ -47,48 +58,90 @@ public abstract class TreeTest {
   /**
    * Tree to test.
    */
-  private Tree<String, String> tree;
+  private Tree<Node, Edge> tree;
+
   /**
-   * Height of the tree.
+   * Height of the test tree.
    */
   private static final int HEIGHT = 3;
+
+  /**
+   * Number of nodes of the test tree.
+   */
+  private static final int NODENUMBER = 7;
+
+  /**
+   * Number of edges of the test tree.
+   */
+  private static final int EDGENUMBER = 5;
+
+  /**
+   * Node table used to build the example tree.
+   */
+  private Table nodeTable;
+
+  /**
+   * Edge table uses to build the example tree.
+   */
+  private Table edgeTable;
 
   /**
    * @see junit.framework.TestCase#setUp()
    */
   @Before
   public void setUp() {
-    tree = this.newInstance();
-    tree.addNode("president");
-    tree.addNode("prime_minister");
-    tree.addNode("minister_one");
-    tree.addNode("minister_two");
-    tree.addNode("minister_three");
-    tree.addNode("secretary_one");
-    tree.addEdge("president-prime-dir",
-        "president", "prime_minister", Graph.EdgeType.DIRECTED);
-    tree.addEdge("prime-min1-dir",
-        "prime_minister", "minister_one", Graph.EdgeType.DIRECTED);
-    tree.addEdge("prime-min2-dir",
-        "prime_minister", "minister_two", Graph.EdgeType.DIRECTED);
-    tree.addEdge("prime-min3-dir",
-        "prime_minister", "minister_three", Graph.EdgeType.DIRECTED);
-    tree.addEdge("min1-sec-dir",
-        "minister_one", "secretary_one", Graph.EdgeType.DIRECTED);
+    Schema nodeSchema = new SchemaImpl();
+    nodeSchema.addColumn("nodeName", String.class, "node_default");
+    Schema edgeSchema = new SchemaImpl();
+    edgeSchema.addColumn("edgeName", String.class, "edge_default");
+    nodeTable = new TableImpl(nodeSchema);
+    edgeTable = new TableImpl(edgeSchema);
+    this.tree = this.newInstance(nodeSchema, edgeSchema);
+    for (int i = 0; i < NODENUMBER; i++) {
+      nodeTable.addRow();
+      nodeTable.set(i, 0, "node_" + String.valueOf(i));
+      this.tree.addNode(new NodeImpl(nodeTable, i));
+    }
+    for (int i = 0; i < EDGENUMBER; i++) {
+      edgeTable.addRow();
+      edgeTable.set(i, "edgeName", "edge_" + String.valueOf(i));
+    }
+    tree.addEdge(new EdgeImpl(edgeTable, 0), new NodeImpl(nodeTable, 0),
+        new NodeImpl(nodeTable, 1), EdgeType.UNDIRECTED);
+    tree.addEdge(new EdgeImpl(edgeTable, 1), new NodeImpl(nodeTable, 0),
+        new NodeImpl(nodeTable, 2), EdgeType.UNDIRECTED);
+    tree.addEdge(new EdgeImpl(edgeTable, 2), new NodeImpl(nodeTable, 1),
+        new NodeImpl(nodeTable, 3), EdgeType.UNDIRECTED);
+    tree.addEdge(new EdgeImpl(edgeTable,3), new NodeImpl(nodeTable, 1),
+        new NodeImpl(nodeTable, 4), EdgeType.UNDIRECTED);
+    tree.addEdge(new EdgeImpl(edgeTable,4), new NodeImpl(nodeTable, 4),
+        new NodeImpl(nodeTable, 5), EdgeType.UNDIRECTED);
   }
 
   /**
    * Creates a suitable instance of tree.
+   * @param nSchema schema for the nodes the tree
+   * @param eSchema schema for the edges of the tree
    * @return suitable tree implementation instance
    */
-  public abstract Tree<String, String> newInstance();
+  public abstract Tree<Node, Edge> newInstance(Schema nSchema, Schema eSchema);
 
   /**
    * @see junit.framework.TestCase#tearDown()
    */
   @After
   public void tearDown() {
-    tree = null;
+    this.tree = null;
+    this.edgeTable = null;
+    this.nodeTable = null;
+  }
+
+  /**
+   * Gets the tree test instance.
+   * @return tree test instance
+   */
+  public Tree<Node, Edge> getTree() {
+    return this.tree;
   }
 
   /**
@@ -96,7 +149,7 @@ public abstract class TreeTest {
    */
   @Test
   public void testGetRoot() {
-    assertEquals("president", tree.getRoot());
+    assertEquals("node_0", this.tree.getRoot().getString("nodeName"));
   }
 
   /**
@@ -104,7 +157,39 @@ public abstract class TreeTest {
    */
   @Test
   public void testGetHeight() {
-    assertEquals(HEIGHT, tree.getHeight());
+    assertEquals(HEIGHT, this.tree.getHeight());
+  }
+
+  /**
+   * Test method for obvious.data.Tree.getChildNodes(Node) method.
+   */
+  @Test
+  public void testGetChildNodes() {
+    assertEquals(2, this.tree.getChildNodes(this.tree.getRoot()).size());
+    assertEquals(2, this.tree.getChildNodes(new NodeImpl(nodeTable, 1)).size());
+    assertEquals(0, this.tree.getChildNodes(new NodeImpl(nodeTable, 2)).size());
+    assertEquals(1, this.tree.getChildNodes(new NodeImpl(nodeTable, 4)).size());
+    Collection<Node> childNode = this.tree.getChildNodes(this.tree.getRoot());
+    for (Node node : childNode) {
+      String nodeName = node.getString("nodeName");
+      assertTrue(nodeName.equals("node_1") || nodeName.equals("node_2"));
+    }
+  }
+
+  /**
+   * Test method for obvious.data.Tree.getChildEdges(Node) method.
+   */
+  @Test
+  public void testGetChildEdges() {
+    assertEquals(2, this.tree.getChildEdges(this.tree.getRoot()).size());
+    assertEquals(2, this.tree.getChildEdges(new NodeImpl(nodeTable, 1)).size());
+    assertEquals(0, this.tree.getChildEdges(new NodeImpl(nodeTable, 2)).size());
+    assertEquals(1, this.tree.getChildEdges(new NodeImpl(nodeTable, 4)).size());
+    Collection<Edge> childEdge = this.tree.getChildEdges(this.tree.getRoot());
+    for (Edge edge : childEdge) {
+      String edgeName = edge.getString("edgeName");
+      assertTrue(edgeName.equals("edge_0") || edgeName.equals("edge_1"));
+    }
   }
 
   /**
@@ -112,9 +197,12 @@ public abstract class TreeTest {
    */
   @Test
   public void testGetDepth() {
-    assertEquals(1, tree.getDepth("prime_minister"));
-    assertEquals(2, tree.getDepth("minister_one"));
-    assertEquals(HEIGHT, tree.getDepth("secratary_one"));
+    assertEquals(0, this.tree.getDepth(this.tree.getRoot()));
+    assertEquals(1, this.tree.getDepth(new NodeImpl(nodeTable, 1)));
+    assertEquals(1, this.tree.getDepth(new NodeImpl(nodeTable, 2)));
+    assertEquals(2, this.tree.getDepth(new NodeImpl(nodeTable, 3)));
+    assertEquals(2, this.tree.getDepth(new NodeImpl(nodeTable, 4)));
+    assertEquals(3, this.tree.getDepth(new NodeImpl(nodeTable, 5)));
   }
 
   /**
@@ -122,9 +210,17 @@ public abstract class TreeTest {
    */
   @Test
   public void testGetParentNode() {
-    assertEquals("minister_one", tree.getParentNode("secretary_one"));
-    assertEquals("prime_minister", tree.getParentNode("minister_one"));
-    assertEquals("president", tree.getParentNode("prime_minister"));
+    assertEquals(null, tree.getParentNode(tree.getRoot()));
+    assertEquals("node_0", tree.getParentNode(new NodeImpl(nodeTable, 1)).get(
+        "nodeName"));
+    assertEquals("node_0", tree.getParentNode(new NodeImpl(nodeTable, 2)).get(
+        "nodeName"));
+    assertEquals("node_1", tree.getParentNode(new NodeImpl(nodeTable, 3)).get(
+        "nodeName"));
+    assertEquals("node_1", tree.getParentNode(new NodeImpl(nodeTable, 4)).get(
+        "nodeName"));
+    assertEquals("node_4", tree.getParentNode(new NodeImpl(nodeTable, 5)).get(
+        "nodeName"));
   }
 
   /**
@@ -132,9 +228,24 @@ public abstract class TreeTest {
    */
   @Test
   public void testGetParentEdge() {
-    assertEquals("min1-sec-dir", tree.getParentEdge("secretary_one"));
-    assertEquals("prime-min1-dir", tree.getParentEdge("minister_one"));
-    assertEquals("president-prime-dir", tree.getParentEdge("prime_minister"));
+    assertEquals("edge_0", tree.getParentEdge(new NodeImpl(nodeTable, 1)).get(
+        "edgeName"));
+    assertEquals("edge_1", tree.getParentEdge(new NodeImpl(nodeTable, 2)).get(
+        "edgeName"));
+    assertEquals("edge_2", tree.getParentEdge(new NodeImpl(nodeTable, 3)).get(
+        "edgeName"));
+    assertEquals("edge_3", tree.getParentEdge(new NodeImpl(nodeTable, 4)).get(
+        "edgeName"));
+    assertEquals("edge_4", tree.getParentEdge(new NodeImpl(nodeTable, 5)).get(
+        "edgeName"));
+  }
+
+  /**
+   * Test method for obvious.data.Tree.getTrees() method.
+   */
+  @Test
+  public void testGetTrees() {
+    assertEquals(this.tree, this.tree.getTrees().iterator().next());
   }
 
 }
