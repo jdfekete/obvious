41ac9b807447c1bf9385130a2298e0e5554c873d
pl <pl@35a52bc0-c39b-11dd-bf30-1748d1f6ef2f>
2010-02-04 15:00:53 +0000

    [obvious-prefuse] Update PrefuseDataFactory. [obvious-prefuse] Add constructor to PrefuseObviousNetwork and PrefuseObviousTable.

--- obvious-prefuse/src/main/java/obvious/prefuse/PrefuseDataFactory.java
+++ obvious-prefuse/src/main/java/obvious/prefuse/PrefuseDataFactory.java
@@ -27,6 +27,8 @@
 
 package obvious.prefuse;
 
+import java.util.Map;
+
 import obvious.ObviousException;
 import obvious.data.DataFactory;
 import obvious.data.Network;
@@ -52,16 +54,96 @@ public class PrefuseDataFactory extends DataFactory {
     return new PrefuseObviousTable(schema);
   }
 
+  /**
+   * Returns a Network instance if the underlyingTable is an instance of
+   * an implementation of Network or a prefuse network. Else, it will rise an
+   * exception.
+   * @param underlyingGraph a candidate Network
+   * @return an Obvious Network
+   * @throws ObviousException if Network creation failed
+   */
   @Override
   public Network wrapGraph(Object underlyingGraph) throws ObviousException {
-    // TODO Auto-generated method stub
-    return null;
+    if (underlyingGraph instanceof Network) {
+      return (Network) underlyingGraph;
+    } else if (underlyingGraph instanceof prefuse.data.Graph) {
+      return new PrefuseObviousNetwork((prefuse.data.Graph) underlyingGraph);
+    } else {
+      throw new ObviousException("Can't create network from this input object");
+    }
   }
 
+  /**
+   * Returns a Table instance if the underlyingTable is an instance of
+   * an implementation of Table or a prefuse Table. Else, it will rise an
+   * exception.
+   * @param underlyingTable a candidate Table
+   * @return an Obvious Table
+   * @throws ObviousException if Table creation failed
+   */
   @Override
-  public Table wrapTable(Object unerlyingTable) throws ObviousException {
-    // TODO Auto-generated method stub
-    return null;
+  public Table wrapTable(Object underlyingTable) throws ObviousException {
+    if (underlyingTable instanceof Table) {
+      return (Table) underlyingTable;
+    } else if (underlyingTable instanceof prefuse.data.Table) {
+      return new PrefuseObviousTable((prefuse.data.Table) underlyingTable);
+    } else {
+      throw new ObviousException("Can't create table from this input object");
+    }
+  }
+
+  /**
+   * Creates a Prefuse Obvious Table from the input parameter.
+   * The parameter 'param' is crucial cause it contains all informations
+   * to build the network.
+   * The map could contain the following keys : "directed" a boolean
+   * indicating if the network is oriented or not, "nodeKey" a string
+   * indicating the column used to identify nodes,
+   * "sourceKey" a string indicating the column used to identify source nodes
+   * in edgeTable, and "targetKey" a string indicating the column used to
+   * identity target nodes in edgeTable.
+   * Extra keys should be simply ignored. If keys are not set, default values
+   * of Prefuse toolkit will be used.
+   * @param name name of the table in the database
+   * @param nodeSchema original obvious schema for the nodes
+   * @param edgeSchema original obvious schema for the edges
+   * @param param a map with specific prefuse graph constructors parameters
+   * @throws ObviousException if Table creation failed
+   * @return a Prefuse Obvious table
+   */
+  @Override
+  public Network createGraph(String name, Schema nodeSchema, Schema edgeSchema,
+      Map<String, Object> param) throws ObviousException {
+    if (!param.containsKey("directed")) {
+      param.put("directed", false);
+    }
+    if (!param.containsKey("sourceKey")) {
+      param.put("sourceKey", prefuse.data.Graph.DEFAULT_SOURCE_KEY);
+    }
+    if (!param.containsKey("targetKey")) {
+      param.put("targetKey", prefuse.data.Graph.DEFAULT_TARGET_KEY);
+    }
+    if (!param.containsKey("nodeKey")) {
+      param.put("nodeKey", prefuse.data.Graph.DEFAULT_NODE_KEY);
+    }
+    return new PrefuseObviousNetwork(nodeSchema, edgeSchema,
+        (Boolean) param.get("directed"), (String) param.get("nodeKey"),
+        (String) param.get("sourceKey"), (String) param.get("targetKey"));
+  }
+
+  /**
+   * Returns an Obvious Table. Obvious Jung tables only use schema
+   * as constructor's parameter.
+   * @param name name of the table
+   * @param schema schema of the table
+   * @param param unused parameter
+   * @return an Obvious Table
+   * @throws ObviousException if table creation failed
+   */
+  @Override
+  public Table createTable(String name, Schema schema,
+      Map<String, Object> param) throws ObviousException {
+    return createTable(name, schema);
   }
 
 }
