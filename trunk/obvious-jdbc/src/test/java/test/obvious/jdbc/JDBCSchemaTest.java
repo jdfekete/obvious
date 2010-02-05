4e4c0f6ece915baed4978d0b961f0dc5316cb65f
pl <pl@35a52bc0-c39b-11dd-bf30-1748d1f6ef2f>
2010-02-05 15:23:17 +0000

    This release has a problem with Maven Test for obvious-jdbc project. It will be fixed in next release. You can still launch the test directly with JUnit4 however. [obvious-jdbc] Major Update for JDBCObviousTable and JDBCObviousSchema. [obvious-jdbc] Update FormatFactorySQL and TypedSQLFormat. [obvious-jdbc] Add JDBCDataFactory. [obvious-jdbc] Add relative tests for JDBCObviousTable and JDBCObviousSchema.

---
+++ trunk/obvious-jdbc/src/test/java/test/obvious/jdbc/JDBCSchemaTest.java
@@ -0,0 +1,111 @@
+/*
+* Copyright (c) 2010, INRIA
+* All rights reserved.
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of INRIA nor the names of its contributors may
+*       be used to endorse or promote products derived from this software
+*       without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package test.obvious.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import obvious.data.Schema;
+import obvious.jdbc.JDBCObviousSchema;
+import test.obvious.data.SchemaTest;
+
+/**
+ * Implementation of  Schema  test-case for JDBCObviousSchema implementation.
+ * @author Pierre-Luc Hemery
+ *
+ */
+public class JDBCSchemaTest extends SchemaTest {
+
+  /**
+   * Driver for the database.
+   */
+  public static final String DRIVER = "com.mysql.jdbc.Driver";
+
+  /**
+   * URL for the test database.
+   */
+  public static final String URL = "jdbc:mysql:///test";
+
+  /**
+   * Login for the test database.
+   */
+  public static final String LOGIN = "root";
+
+  /**
+   * Database for the test database.
+   */
+  public static final String PASSWORD = "";
+
+  /**
+   * Name of the test table.
+   */
+  public static final String TABLE_NAME = "TEST_CASE_TABLE";
+
+  @Override
+  public Schema newInstance() {
+    return new JDBCObviousSchema(DRIVER, URL, LOGIN, PASSWORD, TABLE_NAME);
+  }
+
+  @Override
+  public void tearDown() {
+    try {
+      Class.forName(DRIVER);
+    } catch (ClassNotFoundException e) {
+      System.err.print("ClassNotFoundException: ");
+      System.err.println(e.getMessage());
+    }
+    Connection con = null;
+    PreparedStatement pStatement = null;
+    if (((JDBCObviousSchema) this.getSchema()).tableExist()) {
+      try {
+        con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+        String request = "DROP TABLE " + TABLE_NAME;
+        pStatement = con.prepareStatement(request);
+        pStatement.execute(request);
+      } catch (SQLException e) {
+        System.err.println("SQLException: " + e.getMessage());
+        if (con != null) {
+          try {
+            System.err.print("Transaction is being rolled back");
+            con.rollback();
+          } catch (SQLException ex) {
+            System.err.print("SQLException: ");
+            System.err.println(ex.getMessage());
+          }
+        }
+      } finally {
+        try { pStatement.close(); } catch (Exception e) { e.printStackTrace(); }
+        try { con.close(); } catch (Exception e) { e.printStackTrace(); }
+      }
+    }
+    setSchema(null);
+  }
+
+}
---
+++ trunk/obvious-jdbc/src/test/java/test/obvious/jdbc/JDBCTableTest.java
@@ -0,0 +1,119 @@
+/*
+* Copyright (c) 2010, INRIA
+* All rights reserved.
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of INRIA nor the names of its contributors may
+*       be used to endorse or promote products derived from this software
+*       without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package test.obvious.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import obvious.ObviousException;
+import obvious.data.Schema;
+import obvious.data.Table;
+import obvious.jdbc.JDBCObviousTable;
+import test.obvious.data.TableTest;
+
+/**
+ * Implementation of  Table  test-case for JDBCObviousTable implementation.
+ * @author Pierre-Luc Hemery
+ *
+ */
+public class JDBCTableTest extends TableTest {
+
+  /**
+   * Driver for the database.
+   */
+  public static final String DRIVER = "com.mysql.jdbc.Driver";
+
+  /**
+   * URL for the test database.
+   */
+  public static final String URL = "jdbc:mysql:///test";
+
+  /**
+   * Login for the test database.
+   */
+  public static final String LOGIN = "root";
+
+  /**
+   * Database for the test database.
+   */
+  public static final String PASSWORD = "";
+
+  /**
+   * Name of the test table.
+   */
+  public static final String TABLE_NAME = "TEST_CASE_TABLE";
+
+  /**
+   * Name of the primary key column.
+   */
+  public static final String PRIMARY_KEY = "col2";
+
+  @Override
+  public Table newInstance(Schema schema) throws ObviousException {
+    return new JDBCObviousTable(schema, DRIVER, URL, LOGIN, PASSWORD,
+        TABLE_NAME, PRIMARY_KEY);
+  }
+
+  @Override
+  public void tearDown() {
+    try {
+      Class.forName(DRIVER);
+    } catch (ClassNotFoundException e) {
+      System.err.print("ClassNotFoundException: ");
+      System.err.println(e.getMessage());
+    }
+    Connection con = null;
+    PreparedStatement pStatement = null;
+    if (((JDBCObviousTable) this.getTable()).tableExist()) {
+      try {
+        con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+        String request = "DROP TABLE " + TABLE_NAME;
+        pStatement = con.prepareStatement(request);
+        pStatement.execute(request);
+      } catch (SQLException e) {
+        System.err.println("SQLException: " + e.getMessage());
+        if (con != null) {
+          try {
+            System.err.print("Transaction is being rolled back");
+            con.rollback();
+          } catch (SQLException ex) {
+            System.err.print("SQLException: ");
+            System.err.println(ex.getMessage());
+          }
+        }
+      } finally {
+        try { pStatement.close(); } catch (Exception e) { e.printStackTrace(); }
+        try { con.close(); } catch (Exception e) { e.printStackTrace(); }
+      }
+    }
+    setTable(null);
+  }
+
+}
