eb11a7c5966b8ff1f1309d6d74245878a7a2540f
pl.obvious <pl.obvious@35a52bc0-c39b-11dd-bf30-1748d1f6ef2f>
2010-02-17 13:39:04 +0000

    [obvious] Added a new constructor for EdgeImpl and NodeImpl classes.

--- obvious/src/main/java/obvious/impl/EdgeImpl.java
+++ obvious/src/main/java/obvious/impl/EdgeImpl.java
@@ -28,6 +28,7 @@
 package obvious.impl;
 
 import obvious.data.Edge;
+import obvious.data.Schema;
 import obvious.data.Table;
 
 /**
@@ -47,7 +48,17 @@ public class EdgeImpl extends TupleImpl implements Edge {
     super(tableIn, rowId);
   }
 
-  /*
+  /**
+   * Constructor from a schema with input values.
+   * The values have to be ordered in the correct order for the schema.
+   * @param schema schema for the tuple
+   * @param values value for the tuple
+   */
+  public EdgeImpl(Schema schema, Object[] values) {
+    super(schema, values);
+  }
+
+  /**
    * Indicates if the current edge is equals to this object.
    * It compares the schema, and the value for each column of the schema.
    * @param o test object
