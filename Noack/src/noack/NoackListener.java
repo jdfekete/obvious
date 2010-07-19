fc6b2482417a72299ce7aaa48597e0e6c1915edd
pl.obvious@gmail.com <pl.obvious@gmail.com@35a52bc0-c39b-11dd-bf30-1748d1f6ef2f>
2010-07-19 14:28:53 +0000

    [Noack] Noack has now a maven architecture. [Noack] Some changes to support listening mechanisms.

--- Noack/src/noack/NoackListener.java
+++ Noack/src/noack/NoackListener.java
@@ -1,5 +1,9 @@
 package noack;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class NoackListener implements GraphListener {
 
 
@@ -9,6 +13,12 @@ public class NoackListener implements GraphListener {
    */
   private int inhibitNotify = 0;
 
+  private LinLogLayoutJDBC layout;
+  
+  public NoackListener(LinLogLayoutJDBC layout) {
+    this.layout = layout;
+  }
+  
   @Override
   public void beginEdit() {
     inhibitNotify++;
@@ -23,10 +33,34 @@ public class NoackListener implements GraphListener {
     }
   }
 
+  /**
+   * Exemple function : it works in a very particular case, it tests
+   * severals mechanisms when the new node is in the target position.
+   * Do not use in a generic case.
+   */
   @Override
   public void graphChanged(Graph g, String source, String target, int type) {
     if (inhibitNotify != 0) {
       return;
+    }
+    if (type == EDGE_ADDED) {
+      System.out.println("FIRING CHANGE");
+      Map<Node, double[]> position = layout.getNodePositions();
+      Node targetNode = new Node(target, 1.0f);
+      if (position.containsKey(targetNode) == false) {
+        position.put(targetNode, new double[3]);
+      }
+      position.get(targetNode)[0] = Math.random() - 0.5;
+      position.get(targetNode)[1] = Math.random() - 0.5;
+      position.get(targetNode)[2] = 0.0f;
+      layout.setNodePositions(position);
+      Map<String,Node> nameToNode = layout.makeNodes(layout.getGraphStruct());
+      List<Node> nodes = new ArrayList<Node>(nameToNode.values());
+      List<Edge> edges = layout.makeEdges(layout.getGraphStruct(),nameToNode);
+      System.out.println(nodes.size() + " : " + edges.size());
+      new MinimizerBarnesHut(nodes, edges, 0.0, 1.0, 0.05).
+      minimizeEnergy(layout.getNodePositions(), 10);
+      layout.setNodeCluster(new OptimizerModularity().execute(nodes, edges, false));      
     }
   }
 
