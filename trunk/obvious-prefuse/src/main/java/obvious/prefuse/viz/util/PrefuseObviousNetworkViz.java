99ad57c452f1622110d2b6b88664842b049da689
pl.obvious <pl.obvious@35a52bc0-c39b-11dd-bf30-1748d1f6ef2f>
2010-09-07 08:55:50 +0000

    [obvious-prefuse] Added PrefuseObviousNetworkViz class (first version, second with parameters will follow soon).

---
+++ trunk/obvious-prefuse/src/main/java/obvious/prefuse/viz/util/PrefuseObviousNetworkViz.java
@@ -0,0 +1,109 @@
+/*
+* Copyright (c) 2010, INRIA
+* All rights reserved.
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of INRIA nor the names of its contributors may
+*       be used to endorse or promote products derived from this software
+*       without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package obvious.prefuse.viz.util;
+
+import java.util.Map;
+
+import obvious.data.Network;
+import obvious.data.util.Predicate;
+import obvious.prefuse.viz.PrefuseObviousVisualization;
+import prefuse.action.ActionList;
+import prefuse.action.RepaintAction;
+import prefuse.action.assignment.ColorAction;
+import prefuse.action.assignment.DataColorAction;
+import prefuse.action.layout.graph.ForceDirectedLayout;
+import prefuse.activity.Activity;
+import prefuse.render.DefaultRendererFactory;
+import prefuse.render.LabelRenderer;
+import prefuse.util.ColorLib;
+import prefuse.visual.VisualItem;
+import prefuse.Constants;
+
+/**
+ * PrefuseObviousNetworkViz class.
+ * @author Hemery
+ *
+ */
+public class PrefuseObviousNetworkViz extends PrefuseObviousVisualization {
+	
+	  /**
+	   * X field key.
+	   */
+	  public static final String LABEL_KEY = "label";
+
+	/**
+	 * Constructor.
+	 * @param parentNetwork parent network
+	 * @param predicate a predicate
+	 * @param visName visualization technique name
+	 * @param param parameters
+	 */
+	public PrefuseObviousNetworkViz(Network parentNetwork, Predicate predicate,
+			String visName, Map<String, Object> param) {
+		super(parentNetwork, predicate, visName, param);
+	}
+	
+	@Override
+	protected void initVisualization(Map<String, Object> param) {
+	    prefuse.Visualization prefVis = new prefuse.Visualization();
+	    this.setPrefVisualization(prefVis);
+	    this.getPrefVisualization().add("graph", getPrefuseNetwork());
+	    LabelRenderer r = new LabelRenderer("name");
+	    r.setRoundedCorner(8, 8); // round the corners
+	    this.setRenderer(new PrefuseObviousRenderer(new DefaultRendererFactory(r)));
+	    
+	    // Color for data values.
+	    int[] palette = new int[] {
+	        ColorLib.rgb(255,180,180), ColorLib.rgb(190,190,255)
+	    };
+	    DataColorAction fill = new DataColorAction("graph.nodes", "gender",
+	        Constants.NOMINAL, VisualItem.FILLCOLOR, palette);
+	    ColorAction text = new ColorAction("graph.nodes",
+	        VisualItem.TEXTCOLOR, ColorLib.gray(0));
+	    // Color for edges
+	    ColorAction edges = new ColorAction("graph.edges",
+	        VisualItem.STROKECOLOR, ColorLib.gray(200));
+	    
+	    // Creating the prefuse action list.
+	    ActionList color = new ActionList();
+	    color.add(fill);
+	    color.add(text);
+	    color.add(edges);
+	    // Wrapping the action list around obvious
+	    this.putAction("color", new PrefuseObviousAction(color));
+
+
+	    // Creating a Directed force Layout.
+	   ActionList layout = new ActionList(Activity.INFINITY);
+	   layout.add(new ForceDirectedLayout("graph"));
+	   layout.add(new RepaintAction());
+	   // Wrapping the layout around obvious.
+	   this.putAction("layout", new PrefuseObviousAction(layout));
+	}
+
+}
