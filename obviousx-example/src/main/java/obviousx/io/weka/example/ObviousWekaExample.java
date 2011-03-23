8062acfe7440d574f5b62939b27e8efd86483873
pl.obvious@gmail.com <pl.obvious@gmail.com@35a52bc0-c39b-11dd-bf30-1748d1f6ef2f>
2011-03-23 13:40:52 +0000

    [obviousx-example] Added a clean example for obviousx-weka wrappers for Obvious (linked to a clustering algorithm KMeans).

--- obviousx-example/src/main/java/obviousx/io/weka/example/ObviousWekaExample.java
+++ obviousx-example/src/main/java/obviousx/io/weka/example/ObviousWekaExample.java
@@ -9,13 +9,16 @@ import javax.swing.JFrame;
 import obvious.data.Table;
 import obviousx.ObviousxException;
 import obviousx.io.CSVImport;
+import obviousx.io.weka.ObviousWekaInstances;
 import obviousx.io.weka.ObviousWekaLoader;
 import weka.classifiers.trees.J48;
 import weka.clusterers.ClusterEvaluation;
 import weka.clusterers.SimpleKMeans;
+import weka.core.FastVector;
 import weka.core.Instances;
 import weka.core.Range;
 import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
 import weka.filters.unsupervised.attribute.RemoveType;
 import weka.filters.unsupervised.attribute.StringToNominal;
 import weka.gui.treevisualizer.PlaceNode2;
@@ -31,10 +34,12 @@ public class ObviousWekaExample {
         "src//main/resources//bank-data.csv"), ',');
     Table table = importer.loadTable();
 
+   /*
    ObviousWekaLoader loader = new ObviousWekaLoader(table);
    Instances inst = loader.loadInstances();
-   inst.setClassIndex(6);
-   System.out.println(inst.toSummaryString());
+   inst.setClassIndex(6); */
+   //System.out.println(inst.toSummaryString());
+   
    /*
    System.out.println(inst.toSummaryString());
    System.out.println(inst.meanOrMode(2));
@@ -47,6 +52,13 @@ public class ObviousWekaExample {
    sToN.setInputFormat(inst);
   */
 
+   Instances inst = new ObviousWekaInstances(table, "test",
+        new FastVector(), table.getRowCount());
+   //inst.setClassIndex(2);
+   System.out.println(inst.toSummaryString());
+   System.out.println(inst.meanOrMode(1));
+   System.out.println(inst.variance(1));
+   System.out.println(inst.kthSmallestValue(1, 1));
    ArrayList<Integer> indicesArray = new ArrayList<Integer>();
    for (int i = 0; i < table.getSchema().getColumnCount(); i++) {
      if (table.getSchema().getColumnType(i).equals(String.class)) {
@@ -59,47 +71,20 @@ public class ObviousWekaExample {
      indices[i] = indicesArray.get(i);
    }
 
-   StringToNominal sToN = new StringToNominal();
-   sToN.setAttributeRange(Range.indicesToRangeList(indices));
-   sToN.setInputFormat(inst);
-   Instances sToNInst = Filter.useFilter(inst, sToN);
-
-   RemoveType removeFilter = new RemoveType();
-   String[] options = new String[2];
-   options[0] = "-T";
-   options[1] = "string";
-   removeFilter.setOptions(options);
-   removeFilter.setInputFormat(sToNInst);
-
-   Instances newInst = Filter.useFilter(sToNInst, removeFilter);
-   newInst.deleteAttributeAt(0);
+   Remove remove = new Remove();
+   remove.setAttributeIndicesArray(new int[] {0});
+   remove.setInputFormat(inst);
+   Instances newInst = Filter.useFilter(inst, remove);
    System.out.println(newInst.toSummaryString());
-   //System.out.println(newInst);
-   
-   /*
    SimpleKMeans sKM = new SimpleKMeans();
    sKM.setMaxIterations(16);
-   sKM.setNumClusters(6);
+   sKM.setNumClusters(5);
    ClusterEvaluation eval = new ClusterEvaluation();
    sKM.buildClusterer(newInst);
    eval.setClusterer(sKM);
    eval.evaluateClusterer(new Instances(newInst));
    System.out.println(eval.clusterResultsToString());
    System.out.println(sKM.getSquaredError());
-   */
-   J48 cls = new J48();
-   cls.buildClassifier(newInst);
-   newInst.setClassIndex(4);
-// display tree
-   TreeVisualizer tv = new TreeVisualizer(
-   null, cls.graph(), new PlaceNode2());
-   JFrame jf = new JFrame("Weka Classifier Tree Visualizer: J48");
-   jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-   jf.setSize(800, 600);
-   jf.getContentPane().setLayout(new BorderLayout());
-   jf.getContentPane().add(tv, BorderLayout.CENTER);
-   jf.setVisible(true);
-   // adjust tree
-   tv.fitToScreen();
+
   }
 }
