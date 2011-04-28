b208452f1ccf01a4e2b9f6abd003940f4ef1246e
pl.obvious@gmail.com <pl.obvious@gmail.com@35a52bc0-c39b-11dd-bf30-1748d1f6ef2f>
2011-04-28 08:24:30 +0000

    [obvious-ivtk] Added IvtkObviousForest class.

---
+++ obvious-ivtk/src/main/java/obvious/ivtk/data/IvtkObviousForest.java
@@ -0,0 +1,75 @@
+package obvious.ivtk.data;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import obvious.data.Edge;
+import obvious.data.Forest;
+import obvious.data.Node;
+import obvious.data.Schema;
+import obvious.data.Tree;
+
+/**
+ * Implementation of an Obvious Forest based on Ivtk toolkit.
+ * @author Hemery
+ *
+ */
+public class IvtkObviousForest extends IvtkObviousNetwork
+    implements Forest<Node, Edge> {
+
+  /**
+   * Schema for the nodes.
+   */
+  private Schema nodeSchema;
+
+  /**
+   * Schema for the edges.
+   */
+  private Schema edgeSchema;
+
+  /**
+   * Constructor.
+   * @param nodeSchema an Obvious schema
+   * @param edgeSchema an Obvious schema
+   */
+  public IvtkObviousForest(Schema nodeSchema, Schema edgeSchema) {
+    super(nodeSchema, edgeSchema, true);
+    this.nodeSchema = nodeSchema;
+    this.edgeSchema = edgeSchema;
+  }
+
+  @Override
+  public Collection<Tree<Node, Edge>> getTrees() {
+    Collection<Tree<Node, Edge>> trees = new ArrayList<Tree<Node, Edge>>();
+    Collection<Node> rootNodes = new ArrayList<Node>();
+    for (Node node : getNodes()) {
+      if (getPredecessors(node) == null || getPredecessors(node).size() == 0) {
+        rootNodes.add(node);
+      }
+    }
+    for (Node node : rootNodes) {
+      Tree<Node, Edge> tree = new IvtkObviousTree(nodeSchema, edgeSchema);
+      tree.addNode(node);
+      populateNetwork(tree, node);
+    }
+    return trees;
+  }
+
+  /**
+   * Internal method.
+   * @param tree a Jung tree
+   * @param node an Obvious node
+   */
+  private void populateNetwork(Tree<Node, Edge> tree, Node node) {
+    if (this.getSuccessors(node) != null
+        || this.getSuccessors(node).size() > 0) {
+      for (Node otherNode : this.getSuccessors(node)) {
+        tree.addNode(otherNode);
+        tree.addEdge(getConnectingEdge(node, otherNode), node, otherNode,
+            EdgeType.DIRECTED);
+        populateNetwork(tree, otherNode);
+      }
+    }
+  }
+
+}
